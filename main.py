c3e35395ef2a6f5a0f75d25c6793bb6e4272bbdb
Ruslan Usmanov <ruslan90470@gmail.com>
2020-11-07 00:00:17 +0500

    -Added domain highlighting -Added saving of highlighted domains to a text file

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from random import randint
 
 # Функция генерации электронных почт
 def generateRandomMail(number: int):
-    domains = ['yandex.ru', 'mail.ru', 'ssau.ru', 'gmail.com', 'rambler.ru', 'yahoo.com']
+    domain = ['yandex.ru', 'mail.ru', 'ssau.ru', 'gmail.com', 'rambler.ru', 'yahoo.com']
     mails = []
     for i in range(number):
         result = ""
@@ -13,22 +13,42 @@ def generateRandomMail(number: int):
         for i in range(numberSymbols):
             result += random.choice(string.ascii_letters.lower())
         result += "@"
-        result += random.choice(domains)
+        result += random.choice(domain)
         mails.append(result)
     return mails
 
 
 # Генерация и сохранения электронных почт в текстовый файл с названием "emails.txt"
-number = int(input("Введите количество генерируемых электронных почт: "))
+number = 0
+while number < 50:
+    number = int(input("Введите количество генерируемых электронных почт: "))
 emailsList = generateRandomMail(number)
-print(emailsList)
-emails = open('emails.txt', 'w')
+print("Электронные почты сгенерированы")
+emailsFile = open('emails.txt', 'w')
 for item in emailsList:
-    emails.write("%s\n" % item)
-emails.close()
+    emailsFile.write("%s\n" % item)
+emailsFile.close()
+print("Сгенерированные электронные почты записаны в текстовый файл 'emails.txt'")
+
 
 # Открытие файла с электронными почтами
 emailsFile = open('emails.txt', 'r')
-emailsListForAnalyze = emailsFile.read()
-emailsListForAnalyze = emailsListForAnalyze.splitlines()
-print(emailsListForAnalyze)
+emailsForAnalyze = emailsFile.read()
+emailsForAnalyze = emailsForAnalyze.splitlines()
+print("Текстовый файл 'emails.txt' открыт")
+
+# Выделение доменных имен
+splitedEmails = []
+for element in emailsForAnalyze:
+    splitedEmails.append(element.split('@'))
+domains = []
+for i in range(len(splitedEmails)):
+    domains.append(splitedEmails[i][1])
+print("Выделены домены электронных почт")
+
+# Сохранение доменов в текстовый файл "domains.txt"
+domainsFile = open('domains.txt', 'w')
+for item in domains:
+    domainsFile.write("%s\n" % item)
+domainsFile.close()
+print("Выделенные домены записаны в текстовый файл 'domains.txt'")
